628e62e67e2172bcefef1ec306f531a1dcdec87c
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2023-12-27 20:11:05 +0530

    rating service complete

--- UserService/src/main/java/com/hotel/userservice/UserServiceApplication.java
+++ UserService/src/main/java/com/hotel/userservice/UserServiceApplication.java
@@ -3,8 +3,10 @@ package com.hotel.userservice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class UserServiceApplication {
 
     public static void main(String[] args) {
