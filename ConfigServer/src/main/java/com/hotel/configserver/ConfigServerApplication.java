03c2ef51b18798db0876a8541ec2493190f765ae
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-01-02 12:29:43 +0530

    inti config server

--- ConfigServer/src/main/java/com/hotel/configserver/ConfigServerApplication.java
+++ ConfigServer/src/main/java/com/hotel/configserver/ConfigServerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConfigServerApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ConfigServerApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
 
 }
