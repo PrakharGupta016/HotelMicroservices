b7c39a1f659bc46ac057618ce447410ffb36dc1e
PrakharGupta016 <86834271+PrakharGupta016@users.noreply.github.com>
2024-01-02 12:22:27 +0530

    init

---
+++ ConfigServer/src/test/java/com/hotel/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
